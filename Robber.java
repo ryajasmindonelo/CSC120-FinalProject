61b6ed9dccb9f822abdbb9e446149581460393cf
ryajasmindonelo <145132365+ryajasmindonelo@users.noreply.github.com>
2024-12-06 00:15:11 +0800

    Add files via upload
    robber push

---
+++ Robber.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Robber {
+    private List<String> weapons;
+    private boolean hasDiamond;
+    private int currentRow;
+    private int currentCol;
+    
+
+    public Robber() {
+        this.weapons = new ArrayList<>();
+        this.hasDiamond = false;
+        this.currentRow = 0;
+        this.currentCol = 0;
+    }
+
+    public void grab(String item) {
+        if (weapons.contains(item.toLowerCase())) {
+            System.out.println("You already have " + item + ".");
+        } else {
+            System.out.println("Grabbing " + item);
+            weapons.add(item.toLowerCase());
+        }
+    }
+
+    public int getCurrentRow() {
+        return currentRow;
+    }
+    
+    // Getter for currentCol
+    public int getCurrentCol() {
+        return currentCol;
+    }
+
+    public void attack(String weaponToUse) {
+        if (weapons.contains(weaponToUse.toLowerCase())) {
+            System.out.println("Attacking using " + weaponToUse);
+        } else {
+            System.out.println("You don't have " + weaponToUse + "! You need to grab it first.");
+        }
+    }
+
+    public void moveNorth() {
+        if(currentRow>0) {
+            currentRow--;
+        } else {
+            System.out.println("You've run into a wall!");
+        }
+    }
+
+    public void moveSouth() {
+        if(currentRow<5) {
+            currentRow++;
+        } else {
+            System.out.println("You've run into a wall!");
+        }
+    }
+
+    public void moveEast() {
+        if(currentCol<7) {
+            currentCol++;
+        } else {
+            System.out.println("You've run into a wall!");
+        }
+    }
+
+    public void moveWest() {
+        if(currentCol>0) {
+            currentCol--;
+        } else {
+            System.out.println("You've run into a wall!");
+        }
+    }
+
+    // public boolean walk(String direction) {
+    //     if (direction.equalsIgnoreCase("north") || direction.equalsIgnoreCase("south") ||
+    //         direction.equalsIgnoreCase("east") || direction.equalsIgnoreCase("west")) {
+    //         System.out.println("Walking " + direction);
+    //         return true;
+    //     } else {
+    //         System.out.println("Invalid direction. Please choose: North, South, East, or West.");
+    //         return false;
+    //     }
+    // }
+
+    public void stealDiamond() {
+        if (!hasDiamond) {
+            System.out.println("You successfully steal the Hope Diamond!");
+            this.hasDiamond = true;
+        } else {
+            System.out.println("You already have the Hope Diamond!");
+        }
+    }
+}
