41af241db16344dacdf29d8e725de6f0f2dd6ebe
Your Name <you@example.com>
2024-12-04 19:57:06 -0500

    pushhhhhhh

---
+++ Item.java
@@ -0,0 +1,9 @@
+public class Item {
+    private String name;
+    private int maxDamage;
+
+    public Item(String name, int maxDamage) {
+        this.name = name;
+        this.maxDamage = maxDamage;
+    }
+}
